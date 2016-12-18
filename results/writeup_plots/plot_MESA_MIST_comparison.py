3eab738b0a8eefe5ad647b7a508ca3e9eee85bba
lgbouma <lgbouma@mit.edu>
2016-12-18 12:44:04 -0500

    added plots of the MESA vs MIST comparison for all masses ([Fe/H]=0)

--- results/writeup_plots/plot_MESA_MIST_comparison.py
+++ results/writeup_plots/plot_MESA_MIST_comparison.py
@@ -27,60 +27,64 @@ import read_mist_models
 sun_teff = 5772. # C+2016
 fs = 14
 
-# Get Choi et al published grid solar model
-eep_dir = 'MIST_v1.0_feh_p0.00_afe_p0.0_vvcrit0.4_EEPS/'
-track_name = '00080M.track.eep' # 0.8Msun
-eep = read_mist_models.EEP(eep_dir+track_name)
-print('initial mass: {}'.format(eep.minit))
-print('available columns: {}'.format(eep.hdr_list))
+masses = np.arange(0.4,1.3,0.1)
+mass_list = ['00040','00050','00060','00070','00080','00090','00100','00110','00120']
 
-plt.close('all')
-f, ax = plt.subplots(figsize=(7,5))
-star_age = eep.eeps['star_age']
-star_radius = 10**(eep.eeps['log_R'])
-star_L = 10**(eep.eeps['log_L'])
+for ix, mass in enumerate(masses):
+    # Get Choi et al published grid solar model
+    eep_dir = 'MIST_v1.0_feh_p0.00_afe_p0.0_vvcrit0.4_EEPS/'
+    track_name = mass_list[ix]+'M.track.eep'
+    eep = read_mist_models.EEP(eep_dir+track_name)
+    print('initial mass: {}'.format(eep.minit))
+    #print('available columns: {}'.format(eep.hdr_list))
 
-ax.plot(np.log10(star_age), np.log10(star_radius), c='gold', ls='-', 
-    label='C+16 $R_\star\ [R_\odot]$')
-ax.plot(np.log10(star_age), np.log10(star_L), c='forestgreen', ls='-',
-    label='C+16 $L_\star\ [L_\odot]$')
+    plt.close('all')
+    f, ax = plt.subplots(figsize=(7,5))
+    star_age = eep.eeps['star_age']
+    star_radius = 10**(eep.eeps['log_R'])
+    star_L = 10**(eep.eeps['log_L'])
 
-# Get MESA grid_production_0 ("main to be used" grid)
-table_dir = '../grid_production_0/tables/'
-table_path =  [table_dir+p for p in np.array(os.listdir(table_dir)) if
-    'M0.8_Z0.015' in p][0]
-tab = pd.read_csv(table_path, index_col='age')
+    ax.plot(np.log10(star_age), np.log10(star_radius), c='gold', ls='-', 
+        label='C+16 $R_\star\ [R_\odot]$')
+    ax.plot(np.log10(star_age), np.log10(star_L), c='forestgreen', ls='-',
+        label='C+16 $L_\star\ [L_\odot]$')
 
-ax.plot(np.log10(tab.index), np.log10(tab['R_star']), 
-    c='gold', ls='--',
-    label=r'MESA (production_0) $R_\star\ [R_\odot]$')
-ax.plot(np.log10(tab.index), np.log10(tab['L_star']), 
-    c='forestgreen', ls='--',
-    label=r'MESA (production_0) $L_\star\ [L_\odot]$')
+    # Get MESA grid_production_0 ("main to be used" grid)
+    table_dir = '../grid_production_0/tables/'
+    table_path =  [table_dir+p for p in np.array(os.listdir(table_dir)) if
+        'M'+str(mass)+'_Z0.015' in p][0]
+    tab = pd.read_csv(table_path, index_col='age')
 
-# MESA grid_Choi_repl uses the correct [Fe/H] normalization (I think)
-table_dir = '../grid_Choi_repl/tables/'
-table_path =  [table_dir+p for p in np.array(os.listdir(table_dir)) if
-    'M0.8_Z0.0142' in p][0]
-tab = pd.read_csv(table_path, index_col='age')
+    ax.plot(np.log10(tab.index), np.log10(tab['R_star']), 
+        c='gold', ls='--',
+        label=r'MESA (production_0) $R_\star\ [R_\odot]$')
+    ax.plot(np.log10(tab.index), np.log10(tab['L_star']), 
+        c='forestgreen', ls='--',
+        label=r'MESA (production_0) $L_\star\ [L_\odot]$')
 
-ax.plot(np.log10(tab.index), np.log10(tab['R_star']), c='gold', ls=':',
-    label=r'MESA (Choi repl) $R_\star\ [R_\odot]$')
-ax.plot(np.log10(tab.index), np.log10(tab['L_star']), c='forestgreen', ls=':',
-    label=r'MESA (Choi repl) $L_\star\ [L_\odot]$')
+    # MESA grid_Choi_repl uses the correct [Fe/H] normalization (I think)
+    table_dir = '../grid_Choi_repl/tables/'
+    table_path =  [table_dir+p for p in np.array(os.listdir(table_dir)) if
+        'M'+str(mass)+'_Z0.0142' in p][0]
+    tab = pd.read_csv(table_path, index_col='age')
 
-# Sun age vertical line
-xmin, xmax = 5, 12
-ymin, ymax = -0.8, 0.5
+    ax.plot(np.log10(tab.index), np.log10(tab['R_star']), c='gold', ls=':',
+        label=r'MESA (Choi repl) $R_\star\ [R_\odot]$')
+    ax.plot(np.log10(tab.index), np.log10(tab['L_star']), c='forestgreen', ls=':',
+        label=r'MESA (Choi repl) $L_\star\ [L_\odot]$')
 
-ax.set(xlabel='$\log_{10}$(age [yr])', 
-    ylabel='$\log_{10}$(quantity [unit in legend])',
-    xlim=[xmin, xmax], ylim=[ymin, ymax],
-    title='$0.8M_\odot$. [Fe/H]=0 (n.b. MIST \& MESA have diff calibrations)')
+    # Sun age vertical line
+    xmin, xmax = 5, 12
+    ymin, ymax = -0.8, 0.5
 
-leg = ax.legend(loc='best', fontsize=fs*0.7)
-leg.get_frame().set_facecolor('white')
-leg.get_frame().set_edgecolor('white')
+    ax.set(xlabel='$\log_{10}$(age [yr])', 
+        ylabel='$\log_{10}$(quantity [unit in legend])',
+        xlim=[xmin, xmax], ylim=[ymin, ymax],
+        title=str(mass)+'M$_\odot$. [Fe/H]=0 (n.b. MIST \& MESA have diff calibrations)')
 
-f.tight_layout()
-f.savefig('MESA_MIST_comparison.pdf')
+    leg = ax.legend(loc='best', fontsize=fs*0.7)
+    leg.get_frame().set_facecolor('white')
+    leg.get_frame().set_edgecolor('white')
+
+    f.tight_layout()
+    f.savefig('MESA_MIST_comparison/'+str(mass)+'Msun_MESA_MIST_comparison.pdf')
